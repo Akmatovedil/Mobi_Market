daacce42ac563c559fc07b583b1d74e9865f06fc
Akmatovedil <akmatov.ed@gmail.com>
2023-12-18 00:00:48 +0300

    add registration and auth

--- src/main/java/com/example/Mobi_Market/service/seviceImpl/AuthService.java
+++ src/main/java/com/example/Mobi_Market/service/seviceImpl/AuthService.java
@@ -1,8 +1,9 @@
 package com.example.Mobi_Market.service.seviceImpl;
 
 import com.example.Mobi_Market.dto.JwtRequest;
+import com.example.Mobi_Market.dto.JwtResponse;
 import com.example.Mobi_Market.dto.RegistrationUserDto;
-import com.example.Mobi_Market.dto.RegistrtionResponse;
+import com.example.Mobi_Market.dto.UserDto;
 import com.example.Mobi_Market.entity.User;
 import com.example.Mobi_Market.exceptions.AppError;
 import com.example.Mobi_Market.repository.UserRepository;
@@ -27,36 +28,23 @@ public class AuthService {
 
     public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
         try {
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            authRequest.getUsername(),
-                            authRequest.getPassword()
-                    )
-            );
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
         } catch (BadCredentialsException e) {
-            return new ResponseEntity<>(
-                    new AppError(
-                            HttpStatus.UNAUTHORIZED.value(),
-                            "Неправильный логин или пароль"),
-                    HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(new AppError(HttpStatus.UNAUTHORIZED.value(), "Неправильный логин или пароль"), HttpStatus.UNAUTHORIZED);
         }
-        UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
-        String token = jwtTokenUtils.generateToken(userDetails);
 
-        return ResponseEntity.ok(new RegistrtionResponse(null, userDetails.getUsername(), token));
+        UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
+
+        String token = jwtTokenUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
     }
 
-    public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto) {
-        if (userRepository.findByUsername(registrationUserDto.getUsername()).isPresent()) {
-            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Пользователь с таким именем уже существует"), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<?> createNewUser(@RequestBody RegistrationUserDto registrationUserDto){
+        if (userRepository.findByUsername(registrationUserDto.getUsername()).isPresent()){
+            return new ResponseEntity<>(new AppError(HttpStatus.BAD_REQUEST.value(), "Пользователь с таким именем уже существует"),HttpStatus.BAD_REQUEST);
         }
-
         User user = userService.createNewUser(registrationUserDto);
-
-        UserDetails userDetails = userService.loadUserByUsername(user.getUsername());
-        String token = jwtTokenUtils.generateToken(userDetails);
-
-        return ResponseEntity.ok(new RegistrtionResponse(user.getId(), user.getUsername(), token));
+        return ResponseEntity.ok(new UserDto(user.getId(), user.getUsername(), user.getEmail()));
 
     }
 }
